16e4e543639dd2ffcf2e949a27a68cfbc0cba798
Claudio Maradonna <penguyman@stronzi.org>
2021-06-15 12:42:57 +0200

    fix for bootstrap load_path sprockets

--- lib/jekyll/assets/plugins/bootstrap.rb
+++ lib/jekyll/assets/plugins/bootstrap.rb
@@ -3,7 +3,9 @@
 # Author: Jordon Bedwell
 # Encoding: utf-8
 
-Jekyll::Assets::Utils.activate "bootstrap"
+Jekyll::Assets::Utils.activate "bootstrap" do
+  Bootstrap.load!
+end
 Jekyll::Assets::Utils.activate "bootstrap-sass" do
   Bootstrap.load!
 end
